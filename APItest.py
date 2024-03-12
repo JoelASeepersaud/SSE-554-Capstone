d5d264d53eb74b0546076de97830d5bc4a7de4ab
CMcBride <11056834@live.mercer.edu>
2024-03-12 16:45:47 -0400

    Updated API test to give correct dataframe

--- APItest.py
+++ APItest.py
@@ -1,6 +1,4 @@
 #pip install polygon-api-client
-#pip install plotly
-
 #import modules
 from polygon import RESTClient
 import datetime as dt
@@ -12,16 +10,29 @@ from polygonAPIkey import polygonAPIkey
 # create client with API key 
 client = RESTClient(polygonAPIkey)
 
-stock = 'AAPL'
-
-# daily bars
-testRequest = client.get_aggs(ticker = stock, 
-                              multiplier = 1,
-                              timespan = 'day',
-                              from_ = '2023-12-01',
-                              to = '2100-01-01',
-                              limit = 10)
-
-returnedData = pd.DataFrame(testRequest)
-
-print(returnedData)
+def getStockData(stockTicker, _multiplier, timeWindow, _from, _to, candleLimit):
+    
+    """
+    stockTicker: Ticker symbol for stock. ex. 'APPL'
+    _multiplier: Multiplies with timeWindow. ex. multiplier = 5 & timeWindow = 'minutes' will be 5 minute candles
+    timeWindow: Candle time window. Acceptable: second, minute, hour, day, week, month, quarter, year
+    _from: Start date in YYYY-MM-DD or millisecond timestamp
+    to: End date in YYYY-MM-DD or millisecond timestamp
+    candleLimit: Max number of candles
+    """
+    #Bar Request
+    testRequest = client.get_aggs(ticker = stockTicker,
+                              multiplier = _multiplier,
+                              timespan = timeWindow,
+                              from_ = _from,
+                              to = _to,
+                              limit = candleLimit)
+    returnedData = pd.DataFrame(testRequest)
+    #Convert UNIX timestamps to milliseconds
+    returnedData['Date'] = returnedData['timestamp'].apply(
+                          lambda x: pd.to_datetime(x*1000000)) 
+    #Clean up dataframe
+    returnedData = returnedData.set_index('Date')
+    returnedData.drop(columns = ['vwap', 'otc', 'timestamp'], axis=1, inplace=True)
+    return returnedData
+print(getStockData('AAPL', 1, 'day', '2023-10-01', '2100-01-01', 5))
