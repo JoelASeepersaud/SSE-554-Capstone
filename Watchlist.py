a94e59ed81968ce80b7d43d97206c625f4938dca
JoelASeepersaud <140928189+JoelASeepersaud@users.noreply.github.com>
2024-03-25 19:38:43 -0400

    Add files via upload

--- Watchlist.py
+++ Watchlist.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk as ttk2
 import ttkbootstrap as ttk
 
 #create watchlist display
@@ -14,9 +15,53 @@ def create_watchlist(parent, width, height):
     
     w = width
     h = height
-    text=tk.Label(master = parent, text='testing watchlist')
-    text.pack()
-
+    
+    top_frame = tk.Frame(master = parent, width = w, height = h*.02)
+    top_frame.configure(bg = win_color)
+    top_frame.pack()
+    
+    watchlist_title = tk.Label(master = top_frame, text = 'My Watchlist', font = "Calibri 30")
+    watchlist_title.configure(bg = win_color, fg = 'white')
+    watchlist_title.pack_propagate(False)
+    watchlist_title.pack(pady = (h-h*.118)*.025)
+    
+    info_frame = tk.Frame(master = parent, width = w*.87)
+    info_frame.configure(bg = win_color)
+    info_frame.rowconfigure(0, weight = 1)
+    info_frame.columnconfigure((0,1,2,3,4,5,6,7), weight = 1, uniform = 'a')
+    info_frame.pack(expand = True, fill = 'both', padx = w*.065)
+    
+    name_text = tk.Label(master = info_frame, text = 'Name', font = "Calibri 15")
+    name_text.configure(bg = win_color, fg = 'white')
+    name_text.grid(row = 0, column = 0, columnspan = 4, sticky = 'w')
+    
+    price_text = tk.Label(master = info_frame, text = 'Price', font = "Calibri 15")
+    price_text.configure(bg = win_color, fg = 'white')
+    price_text.grid(row = 0, column = 4, sticky = 'e')
+    
+    change_text = tk.Label(master = info_frame, text = 'Change', font = "Calibri 15")
+    change_text.configure(bg = win_color, fg = 'white')
+    change_text.grid(row = 0, column = 5, sticky = 'e')
+    
+    changepercent_text = tk.Label(master = info_frame, text = 'Change %', font = "Calibri 15")
+    changepercent_text.configure(bg = win_color, fg = 'white')
+    changepercent_text.grid(row = 0, column = 6, sticky = 'e')
+    
+    date_text = tk.Label(master = info_frame, text = 'Date', font = "Calibri 15")
+    date_text.configure(bg = win_color, fg = 'white')
+    date_text.grid(row = 0, column = 7, sticky = 'e')
+    
+    divider_frame = tk.Frame(master = parent, width = w*.87, height = 4)
+    divider_frame.configure(bg = top_bar_color)
+    divider_frame.pack(expand = True, fill = 'x', padx = w*.065)
+    
+    bot_frame = tk.Frame(master = parent, width = w, height = (h-h*.118)*.83)
+    bot_frame.pack_propagate(False)
+    bot_frame.pack(side = 'bottom')
+    
+    text_list = [('1 S&P500', '2', '1', '50%', '5/2/2024'), ('2 S&P500', '2', '1', '50%', '5/2/2024'), ('3 S&P500', '2', '1', '50%', '5/2/2024'), ('4 S&P500', '2', '1', '50%', '5/2/2024'), ('5 S&P500', '2', '1', '50%', '5/2/2024'), ('6 S&P500', '2', '1', '50%', '5/2/2024'), ('7 S&P500', '2', '1', '50%', '5/2/2024'), ('8 S&P500', '2', '1', '50%', '5/2/2024'), ('9 S&P500', '2', '1', '50%', '5/2/2024'), ('2 S&P500', '2', '1', '50%', '5/2/2024'), ('3 S&P500', '2', '1', '50%', '5/2/2024'), ('4 S&P500', '2', '1', '50%', '5/2/2024'), ('5 S&P500', '2', '1', '50%', '5/2/2024'), ('6 S&P500', '2', '1', '50%', '5/2/2024'), ('7 S&P500', '2', '1', '50%', '5/2/2024'), ('8 S&P500', '2', '1', '50%', '5/2/2024'), ('9 S&P500', '2', '1', '50%', '5/2/2024')]
+    list_frame = ListFrame(bot_frame, text_list, 70)
+    
 #create scrollable list
 class ListFrame(tk.Frame):
     
