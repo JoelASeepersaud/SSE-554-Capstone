b1ae235abb4d113bd3347a27542d1ff679c2f174
CMcBride <11056834@live.mercer.edu>
2024-03-31 07:15:59 -0400

    Node Creation

--- dataHandle.py
+++ dataHandle.py
@@ -9,6 +9,18 @@ from polygonAPIkey import polygonAPIkey
 
 # create client with API key 
 client = RESTClient(polygonAPIkey)
+class Node(object):
+    def __init__(self, dataFrame, row):
+        self.ticker = dataFrame.loc[row,"ticker"]
+        self.open = dataFrame.loc[row,"open"]
+        self.high = dataFrame.loc[row,"high"]
+        self.low = dataFrame.loc[row,"low"]
+        self.close = dataFrame.loc[row,"close"]
+        self.volume = dataFrame.loc[row,"volume"]
+
+
+
+
 
 def cleanDataSingle(dataFrame):
     #Convert UNIX timestamps to milliseconds
@@ -18,15 +30,26 @@ def cleanDataSingle(dataFrame):
     dataFrame = dataFrame.set_index('Date')
     dataFrame.drop(columns = ['vwap', 'otc', 'timestamp', 'transactions'], axis=1, inplace=True)
     return dataFrame
+    """cleanDataSingle(pd.DataFrame(client.get_aggs("AAPL",
+        1,
+        "minute",
+        "2022-01-01",
+        "2023-02-03",
+        limit=10,)))"""
+
+def cleanDataAll(dataFrame, volumeMin):
+    for x in dataFrame.index:
+        if dataFrame.loc[x, "volume"] <  volumeMin:
+            dataFrame.drop(x, inplace = True)
+    dataFrame.drop(columns = ['vwap', 'otc', 'timestamp', 'transactions'], axis=1, inplace=True)
+    dataFrame.reset_index()
+    """
+    test = pd.DataFrame(client.get_grouped_daily_aggs("2023-03-16"))
+    cleanDataAll(test, 10000)
+    print (test)
+    """
 
 """
-print(cleanDataSingle(client.get_aggs("AAPL",
-    1,
-    "minute",
-    "2022-01-01",
-    "2023-02-03",
-    limit=10,)))
-
 Examples:
 
 Single Stock:
